fb13213fbd3c4625192fb30e074adecfaf9387c2
Antonio Augello <toni.gello94@gmail.com>
2019-04-23 08:14:47 +0000

    boardgame.js file update and working now, start button added, new board set to levelup on level two js file

--- assets/js/boardgame.js
+++ assets/js/boardgame.js
@@ -13,108 +13,83 @@ Array.prototype.memory_card_shuffle = function() {
     }
 }
 
+
 //.................................Starting Game BTN//
 function startGame() {
     var x = document.getElementById("boardgame");
     if (x.style.display === "none") {
         x.style.display = "block";
     }
-    document.getElementById("startBtn").onclick = function() { newBoard()};
-    document.getElementById("boardgame").style.display = "block";
+    
+    //delete//
+    $.getScript("/assets/js/timer.js", function( data, textStatus, jqxhr ) {
+});
+    document.getElementById("startBtn").onclick = function() { newBoard() };
+    document.getElementById("boardgame").style.display = "inline-block";
 }
 
-
 //...........................................Game starting board//
-    function newBoard() {
-        card_flipped = 0;
-        var output = '';
-        memory_array.length = [4]; // Rememnber to change this value!!//
-        memory_array.memory_card_shuffle();
-        for (var i = 0; i < memory_array.length; i++) {
-            output += '<div id="card_' + i + '" onclick="memoryFlipCard(this,\'' + memory_array[i] + '\')"></div>';
-        }
-        document.getElementById('boardgame').innerHTML = output;
+function newBoard() {
+    card_flipped = 0;
+    var output = '';
+    //...................Using this newArray and changing it here it has as result undefided values for the array//
+    memory_array.length = [4];
+    memory_array.memory_card_shuffle();
+    for (var i = 0; i < memory_array.length; i++) {
+        output += '<div id="card_' + i + '" onclick="memoryFlipCard(this,\'' + memory_array[i] + '\')"></div>'
     }
+    document.getElementById('boardgame').innerHTML = output;
+}
 
-    function levelTwoBoard() {
-        var memory_array = newMemoryArray;
-        card_flipped = 0;
-        var output = '';
-        memory_array.length = [18];
-        memory_array.memory_card_shuffle();
-        for (var i = 0; i < memory_array.length; i++) {
-            output += '<div id="card_' + i + '" onclick="memoryFlipCard(this,\'' + memory_array[i] + '\')"></div>';
+//.............................................................. To fix why the array's values after the 11th element are udefined//
+function levelTwoBoard() {
+    $.getScript("/assets/js/levels/levelTwo.js", function( data, textStatus, jqxhr ) {
+});
+}
+
+function memoryFlipCard(card, val) {
+
+    if (card.innerHTML == "" && memory_values.length < 2) {
+        card.style.background = '#FFF';
+        card.innerHTML = val;
+        if (memory_values.length == 0) {
+            memory_values.push(val);
+            memory_card_ids.push(card.id);
         }
-        document.getElementById('boardgame').innerHTML = output;
-    }
-
-    function levelThreeBoard() {
-        card_flipped = 0;
-        var output = '';
-        var i = [0];
-        memory_array.length = [16];
-        memory_array.memory_card_shuffle();
-        for (var i = 0; i < memory_array.length; i++) {
-            output += '<div id="card_' + i + '" onclick="memoryFlipCard(this,\'' + memory_array[i] + '\')"></div>';
-        }
-        document.getElementById('boardgame').innerHTML = output;
-    }
-
-    function memoryFlipCard(card, val) {
-
-        if (card.innerHTML == "" && memory_values.length < 2) {
-            card.style.background = '#FFF';
-            card.innerHTML = val;
-            if (memory_values.length == 0) {
-                memory_values.push(val);
-                memory_card_ids.push(card.id);
+        else if (memory_values.length == 1) {
+            memory_values.push(val);
+            memory_card_ids.push(card.id);
+            if (memory_values[0] == memory_values[1]) {
+                card_flipped += 2;
+                // Clear both arrays
+                memory_values = [];
+                memory_card_ids = [];
+                // Check to see if the whole board is cleared
+                if (card_flipped == memory_array.length) {
+                    alert("Well done! But this one was an easy one, let's go ahead! Level Two Starting!");
+                    document.getElementById('boardgame').innerHTML = "";
+                    levelTwoBoard();
+                } //--------------------------------------------------------Stop this condition and start a new one//
             }
-            else if (memory_values.length == 1) {
-                memory_values.push(val);
-                memory_card_ids.push(card.id);
-                if (memory_values[0] == memory_values[1]) {
-                    card_flipped += 2;
+            else {
+                function flip2Back() {
+                    // Flip the 2 cards back over
+                    var card_1 = document.getElementById(memory_card_ids[0]);
+                    var card_2 = document.getElementById(memory_card_ids[1]);
+                    card_1.style.cssText = 'background: url(/assets/img/card.png) no repeat, background-size: cover';
+                    card_1.innerHTML = "";
+                    card_2.style.cssText = 'background: url(/assets/img/card.png) no repeat, background-size: cover';
+                    card_2.innerHTML = "";
                     // Clear both arrays
                     memory_values = [];
                     memory_card_ids = [];
-                    // Check to see if the whole board is cleared
-                    if (card_flipped == memory_array.length) {
-                        alert("Well done! Level Two Starting!");
-                        document.getElementById('boardgame').innerHTML = "";
-                        levelTwoBoard();
-                    }
-                    if (card_flipped == memory_array.length) {
-                        alert("Well done! Level Three Starting!");
-                        document.getElementById('boardgame').innerHTML = "";
-                        levelThreeBoard();
-                    }
-                    if (card_flipped == memory_array.length) {
-                        alert("WIN!");
-                        document.getElementById('boardgame').innerHTML = "";
-                        levelThreeBoard();
-                    }
                 }
-                else {
-                    function flip2Back() {
-                        // Flip the 2 cards back over
-                        var card_1 = document.getElementById(memory_card_ids[0]);
-                        var card_2 = document.getElementById(memory_card_ids[1]);
-                        card_1.style.cssText = 'background: url(/assets/img/card.jpg) no repeat, background-size: cover';
-                        card_1.innerHTML = "";
-                        card_2.style.cssText = 'background: url(/assets/img/card.jpg) no repeat, background-size: cover';
-                        card_2.innerHTML = "";
-                        // Clear both arrays
-                        memory_values = [];
-                        memory_card_ids = [];
-                    }
-                    setTimeout(flip2Back, 700);
-                }
+                setTimeout(flip2Back, 700);
             }
         }
-
-
     }
 
-    window.addEventListener(newBoard());
-    window.addEventListener(levelTwoBoard());
-    window.addEventListener(levelThreeBoard());
+
+}
+
+window.addEventListener(newBoard());
