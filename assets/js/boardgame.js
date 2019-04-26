7e317d1458f1ea18e0ebac7df0a8a67391708ff4
Antonio Augello <toni.gello94@gmail.com>
2019-04-26 13:14:49 +0000

    added new functions and addedd comments for each function

--- assets/js/boardgame.js
+++ assets/js/boardgame.js
@@ -7,10 +7,14 @@ var memory_values = [];
 var memory_card_ids = [];
 var card_flipped = 0;
 var num_cards = 4;
+var totalPoints = 0;
+var match = 0;
+var level = 1;
+var click = 0;
 //Array.prototype.memory_card_shuffle = function() {
-  //  var i = this.length,
-    //    j, temp;
-    //while (--i > 0) {
+//  var i = this.length,
+//    j, temp;
+//while (--i > 0) {
 //        j = Math.floor(Math.random() * (i + 1));
 //        temp = this[j];
 //        this[j] = this[i];
@@ -62,9 +66,25 @@ function getRandomInt(min, max) {
 function startGame() {
     var x = document.getElementById("boardgame");
     if (x.style.display === "none") {
-        x.style.display = "block";
+        x.style.display == "block";
     }
     document.getElementById("startBtn").onclick = function() { newBoard(num_cards) };
+    startTimer();
+    showBoardGame();
+}
+
+function showBoardGame() {
+    $("#startBtn").click(function() { $(".boardgame").show(); });
+}
+
+
+// Function that will restart the sounds 
+function startSounds() {}
+
+//Function that change my sound button class on each click
+function switchSoundClass() {
+    $("#soundOnOffBtn").toggleClass('fas fa-volume-mute fas fa-volume-up');
+    document.getElementById("soundOnOffBtn").onclick = function() { switchSoundClass() };
 }
 
 
@@ -109,33 +129,40 @@ function newBoard(num_cards) {
 //}
 
 
-//....................................................Flip card function//
+/*....................................................Flip card function
+It will assign an image value to the card and then it will check if my none of the cards are flipped
+it will assign to the card i clicked the value */
+
 function memoryFlipCard(card, val) {
     if (card.innerHTML == "" && memory_values.length < 2) {
         card.style.background = '#FFF';
-        //card.innerHTML = getElementById("imgCard");
         card.innerHTML = '<img id="imgCard" class="backImg" src="/assets/img/cards/' + val + '.png"/>'
-        //setTimeout(function() { $("img").fadeOut(); }, 1000); //.............function that fade out my images working but on the image after clicking //
         if (memory_values.length == 0) {
+            totalClick();
             memory_values.push(val);
             memory_card_ids.push(card.id);
-            // .............. setTimeout(function() { $(card).fadeOut(); }, 1000); // if here it delete my card (nice effect to do for next levels //
         }
+        //function that will run only if i have a card flipped
         else if (memory_values.length == 1) {
+            totalClick();
             memory_values.push(val);
             memory_card_ids.push(card.id);
+            //And when i click another card it will add the value to the other card and it will check if it is a match
             if (memory_values[0] == memory_values[1]) {
+                showMatch();
                 card_flipped += 2;
-                // Clear both arrays
+                // If it is a match it will clear both arrays and the function can restart
                 memory_values = [];
                 memory_card_ids = [];
-                // Check to see if the whole board is cleared
+                // If all the card on the board are flipped then it generate a newboard  
                 if (card_flipped == shuffled.length) {
                     document.getElementById('boardgame').innerHTML = "";
                     generateNewBoard();
-                    // levelTwoBoard();
-                } //--------------------------------------------------------Should I Stop this condition and start a new one?//
+                    showLevel(level += 1);
+
+                }
             }
+            //If the id of the selected card is not the same then it will flip back the card assigning them my card logo 
             else {
                 function flip2Back() {
                     // Flip the 2 cards back over
@@ -155,15 +182,16 @@ function memoryFlipCard(card, val) {
     }
 }
 
-//Loop that will let my num_cards add 4 new items each time until value of 16 reached
+//Loop that will let my num_cards add 4 new items each time until value of 16 reached (bug= it show 4 more card than my )
 function generateNewBoard() {
     newBoard(num_cards + 4);
-    if (num_cards === 8) {
-    $('#levelFiveModal').modal({ show: true });
+    if (num_cards >= 16) {
+        $('#levelFiveModal').modal({ show: true });
         startLevelFive();
+        num_cards = [];
     }
     else {
-        $('#levelTwoModal').modal({show: true});
+        $('#levelTwoModal').modal({ show: true });
         num_cards += 4;
     }
 }
@@ -171,37 +199,39 @@ function generateNewBoard() {
 
 function startLevelFive(card, val) {
     console.log("level 5 will start now");
-    showCards();
+    // showCards(); to activate later
 }
 
 
 //..............................................New function that allow to see the cards for 10 seconds //
+/*
 function showCards() {
     getElementByClass('.backLogoCardDiv.backImg');
-    
-    
-    if (card_flipped == 0){
-        
+
+
+    if (card_flipped == 0) {
+
     }
     setTimeout(function() { $("img").fadeOut(); }, 1000);
 }
-
+*/
 
 //On Page load modal 
-$(window).on('load',function(){
-        $('#onLoadModal').modal('show');
-    });
+$(window).on('load', function() {
+    $('#onLoadModal').modal('show');
+});
 
 
 //game timer
 var second = 0,
     minute = 0;
-var timer = document.querySelector(".timer");
+var timer = document.querySelector("#timer");
 var interval;
 
+// Timer set to start when click first card (BUG = time go faster the double for each time the loop start again, so fo each level)
 function startTimer() {
     interval = setInterval(function() {
-        timer.innerHTML = minute + "mins " + second + "secs";
+        timer.innerHTML = "Timer:" + minute + "mins " + second + "secs";
         second++;
         if (second == 60) {
             minute++;
@@ -214,4 +244,46 @@ function startTimer() {
     }, 1000);
 }
 
+
+// Function that should stop the reloop of the startTimer function
+function timeGaming() {
+
+}
+
+/*function that will count the point the user does for each match ==> replace this function here and use it to value the totale times that the
+user click on the card, then made another function that will remove the totalClick */
+function showMatch() {
+    let cardMatched = ("Matches: " + match);
+    if (card_flipped >= 0) {
+        match++;
+    }
+    //var totalPoints =  ;
+
+    $('#point').text(cardMatched);
+    console.log(match);
+}
+
+function totalClick() {
+    let cardClicked = ("Total Click: " + click);
+    if (card_flipped >= 0) {
+        click++;
+    }
+    $('#totalClick').text(cardClicked);
+    console.log(point);
+}
+
+//Function that will show the actual level 
+function showLevel() {
+    let actualLevel = ("Level: " + level);
+    if (card_flipped == shuffled.length) {
+        level++;
+    }
+    $('#level').text(actualLevel);
+    console.log(level);
+}
+
+
 newBoard(num_cards);
+showMatch();
+totalClick()
+showLevel();
